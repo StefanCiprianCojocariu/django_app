25d4610bd99a5445d3def099c2d64194380bc9a9
StefanCiprianCojocariu <58269934+StefanCiprianCojocariu@users.noreply.github.com>
2021-10-13 21:01:05 +0300

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -76,17 +76,6 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
