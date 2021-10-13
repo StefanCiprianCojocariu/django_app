4950e9ac7fc31de62b7f72a05a4c4237ef2047c8
StefanCiprianCojocariu <58269934+StefanCiprianCojocariu@users.noreply.github.com>
2021-10-13 21:03:57 +0300

    Update settings.py

--- django_app/settings.py
+++ django_app/settings.py
@@ -75,6 +75,11 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+        'default' : {
+            'ENGINE': 'django.db.backends.sqlite3'
+        }
+}
 
 
 # Password validation
